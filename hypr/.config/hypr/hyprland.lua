07e5b2486ea56d08b848c43f852177a955f37029
LastElaphine <shailen.s.patel@gmail.com>
2026-09-05 16:58:19 -0700

    Remove padding between windows and waybar

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -87,10 +87,10 @@ hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
 	general = {
-		gaps_in = 5,
-		gaps_out = 5,
+		gaps_in = 0,
+		gaps_out = 0,
 
-		border_size = 1,
+		border_size = 0,
 
 		col = {
 			active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
